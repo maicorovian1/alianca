377149a8f7c08581691f106ad6c8a97fb388d54c
maicorovian1 <154009277+maicorovian1@users.noreply.github.com>
2024-04-04 10:19:38 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+document.querySelectorAll('.nav-link').forEach(item => {
+    item.addEventListener('click', event => {
+        event.preventDefault();
+        const targetId = event.target.getAttribute('href');
+        document.querySelector(targetId).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+const btnScrollToTop = document.getElementById("btnScrollToTop");
+
+btnScrollToTop.addEventListener("click", function () {
+    window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+});
+
+window.addEventListener("scroll", function () {
+    if (window.scrollY >= 100) {
+        btnScrollToTop.classList.add("show");
+    } else {
+        btnScrollToTop.classList.remove("show");
+    }
+});
+
+const visitorCountElement = document.getElementById("visitorCount");
+
+if (localStorage.getItem("visitorCount")) {
+    visitorCountElement.textContent = localStorage.getItem("visitorCount");
+} else {
+    visitorCountElement.textContent = "0";
+}
+
+const currentVisitorCount = parseInt(visitorCountElement.textContent) + 1;
+visitorCountElement.textContent = currentVisitorCount;
+localStorage.setItem("visitorCount", currentVisitorCount);
